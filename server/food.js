369461f465de8c9e8a9d33aec101c7d6c0a1873c
Emmanuel436 <72251592+Emmanuel436@users.noreply.github.com>
2021-12-16 00:06:51 +0000

    working app

---
+++ server/food.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const FoodSchema = new mongoose.Schema({
+  foodName: {
+    type: String,
+    required: true,
+  },
+  daysSinceEaten: {
+    type: Number,
+    required: true,
+  },
+});
+
+const FoodModel = mongoose.model("Food", FoodSchema);
+
+export default FoodModel;
---
+++ server/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import FoodModel from "./food.js";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.post("/insert", async (req, res) => {
+  const nameOfFood = req.body.foodName;
+  const days = req.body.days;
+
+  const food = new FoodModel({ foodName: nameOfFood, daysSinceEaten: days });
+  try {
+    await food.save();
+    console.log(food);
+    // res.send("Thank you gamer");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await FoodModel.findByIdAndDelete(id).exec();
+  res.send("item deleted");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
